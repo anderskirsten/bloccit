e0cc7178f54342a993494f25d2459d60af3a7d3c
Kirsten Anderson <anders.kirsten@gmail.com>
2017-09-08 17:47:05 +0000

    Checkpoint 32: in progress

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,9 +5,11 @@ class Post < ActiveRecord::Base
     has_many :votes, dependent: :destroy
     has_many :favorites, dependent: :destroy
     
-    after_create :create_vote, :create_favorite
+    #after_create :create_vote, :create_favorite
     
     default_scope { order('rank DESC') }
+    
+    scope :visible_to, -> (user) { user ? all : joins(:topic).where('topics.public' => true) }
     
     validates :title, length: { minimum: 5 }, presence: true
     validates :body, length: { minimum: 20 }, presence: true
